5a748d027454850432bd14e4d8bcd495e1a83018
Egor <45356764+egoraleksandrov1ch@users.noreply.github.com>
2019-03-28 17:31:12 +0300

    any_message_here

--- src/main.js
+++ src/main.js
@@ -9,8 +9,15 @@ export default class Main extends React.Component {
         super(props)
         this.state = {
             offTop: null,
+            menu: true,
         };
         this.scrollPage = this.scrollPage.bind(this);
+        this.openMenu = this.openMenu.bind(this);
+    };
+    openMenu () {
+        this.setState( {
+            menu: !this.state.menu,
+        });
     };
     scrollPage () {
         this.setState( {
@@ -31,7 +38,6 @@ export default class Main extends React.Component {
         if (this.state.offTop <= 1) {
             hieghtNav = '120px';
             pos = '';
-            hieghtLink = '50px';
             numbOrBtn = <div className='number'>
                 <a href='+375291234567'>+375 (29) 123-45-67</a><br/>
                 <a href='+375331234567'>+375 (33) 123-45-67</a>
@@ -53,15 +59,16 @@ export default class Main extends React.Component {
                     <div className='headercomponent' style={ {height: hieghtNav, position: pos} }>
                         <header>
                             <img src='./img/logo.png' />
-                            <nav>
-                                <Link style={ {paddingTop: hieghtLink} } to='/'>главная</Link>
-                                <Link style={ {paddingTop: hieghtLink} } to='/portfolio'>портфолио</Link>
-                                <Link style={ {paddingTop: hieghtLink} } to='/advantages'>преимущества</Link>
-                                <Link style={ {paddingTop: hieghtLink} } to='/schemeofwork'>схема работы</Link>
-                                <Link style={ {paddingTop: hieghtLink} } to='/reviews'>отзывы</Link>
-                                <Link style={ {paddingTop: hieghtLink} } to='/contacts'>контакты</Link>
+                            <nav style={ {display:this.state.menu ? 'none' : 'flex'} }>
+                                <Link onClick={this.openMenu} style={ {paddingTop: hieghtLink} } to='/'>главная</Link>
+                                <Link onClick={this.openMenu} style={ {paddingTop: hieghtLink} } to='/portfolio'>портфолио</Link>
+                                <Link onClick={this.openMenu} style={ {paddingTop: hieghtLink} } to='/advantages'>преимущества</Link>
+                                <Link onClick={this.openMenu} style={ {paddingTop: hieghtLink} } to='/schemeofwork'>схема работы</Link>
+                                <Link onClick={this.openMenu} style={ {paddingTop: hieghtLink} } to='/reviews'>отзывы</Link>
+                                <Link onClick={this.openMenu} style={ {paddingTop: hieghtLink} } to='/contacts'>контакты</Link>
                             </nav>
                             {numbOrBtn}
+                            <i className="fas fa-align-justify" onClick={this.openMenu}></i>
                         </header>
                     </div>
                     {/* <Route exact path="/" component={HomePage} /> */}
